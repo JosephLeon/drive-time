8c8c7e948d06099398e0e37a87816413ae03c9aa
Joseph Leon <joseph.leon.9@gmail.com>
2014-06-20 10:05:50 -0500

    Initial push to repo

---
+++ calculate_drive_time.js
@@ -0,0 +1,58 @@
+(function ($) {
+  Drupal.behaviors.calculate_drive_time = {
+    attach: function (context, settings) {
+      // Your Javascript code goes here
+
+      var originField = '#origin-field' // Field you want the origin pulled from
+      var targetField = '#result-jquery-field' //Field you would like to have distance appear inside
+
+      $(originField).keypress(function(e){
+        if(e.which == 13) {
+          var directionsService = new google.maps.DirectionsService();
+          var destination = $(originField).val();
+          var request = {
+            origin: startingAddress,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING
+          };
+          directionsService.route(request, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                $(targetField).text(result.routes[0].legs[0].duration.text).slideDown();
+            }
+            else {
+              if(destination.length!=0) {
+                $(targetField).text('Address is invalid!').slideDown();
+              }
+              else {
+                $(targetField).text("Please input address!").slideDown();
+              }
+            }
+          });
+        }
+      });
+
+      $('.block-calculate-drive-time .calculate-button', context).click(function () {
+        var directionsService = new google.maps.DirectionsService();
+        var destination = $(originField).val();
+        var request = {
+          origin: startingAddress,
+          destination: destination,
+          travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function(result, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              $(targetField).text(result.routes[0].legs[0].duration.text).slideDown();
+          }
+          else {
+            if(destination.length!=0) {
+              $(targetField).text('Address is invalid!').slideDown();
+            }
+            else {
+              $(targetField).text("Please input address!").slideDown();
+            }
+          }
+        });
+      });
+    }
+  };
+}(jQuery));
